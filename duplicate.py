a2ff012884af0dbbd119d65ee807807e50250cf4
SrinuCh6 <98872429+SrinuCh6@users.noreply.github.com>
2022-02-02 14:54:37 +0530

    latest file
    latest file

--- duplicate.py
+++ duplicate.py
@@ -3,7 +3,7 @@ arr = [1, 2, 3, 2, 1];
 
 print("Duplicate elements in given array: ");
 # Searches for duplicate element
-for i in range(0, len(arr)):
+for i in range(1, len(arr)):
     for j in range(i + 1, len(arr)):
         if (arr[i] == arr[j]):
             print(arr[j]);
